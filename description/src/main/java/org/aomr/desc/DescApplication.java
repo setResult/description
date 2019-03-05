6716c82720d1c55e4fced6cfd4c598d0e6a10f39
aomr <648058540@qq.com>
2019-03-05 10:30:10 +0800

    Initial commit

---
+++ description/src/main/java/org/aomr/desc/DescApplication.java
@@ -0,0 +1,13 @@
+package org.aomr.desc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DescApplication.class, args);
+    }
+
+}
